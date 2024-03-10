1fae7a98839aea43a515a8f82d0368c2f34ccbc6
2519 <2519167027@qq.com>
2024-03-10 17:23:43 +0800

    给栈的版本1添加了注释

--- Src/MyStack.hpp
+++ Src/MyStack.hpp
@@ -14,19 +14,27 @@ namespace MyStack {
     class FixedSizeStack {
     private:
 
-        uint32_t capacity;
-        uint32_t size;
-        T *data;
+        uint32_t capacity;//表示栈的最大容量，即能容纳元素的最大数量。
+        uint32_t size;//表示当前栈中元素的数量。
+        T *data;//一个动态分配的数组，用于存储栈中的元素。
 
     public:
+        /*
+         *  根据传入的参数初始化栈，创建一个容量为capa的数组，并将size设为0。
+         */
         FixedSizeStack(uint32_t capa) : capacity(capa), size(0), data(new T[capacity]) {};
 
+        /*
+        * 在栈对象生命周期结束时调用，负责释放之前动态分配的数组内存。
+        */
         ~FixedSizeStack() {
             delete[]data;
         }
-
+        /*
+         * 防止拷贝构造和赋值操作发生，这是因为栈内部管理了动态内存，直接拷贝可能导致资源泄露或逻辑错误。
+         */
         FixedSizeStack(const FixedSizeStack &) = delete;
-
+        FixedSizeStack(const FixedSizeStack &&) = delete;
         FixedSizeStack &operator=(const FixedSizeStack &) = delete;
 
     public:
